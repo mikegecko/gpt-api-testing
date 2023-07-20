451fdadd4cdfb616d8603713c39f2c5f14254a4a
Michael Guedko <mjgeko@gmail.com>
2023-07-20 14:39:23 -0400

    Add methods for auth

---
+++ controllers/authController.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+    createUser: async (req, res, next) => {
+        try{
+
+        }
+        catch(err){
+
+        }
+    },
+    loginUser: async (req, res, next) => {
+        try{
+
+        }
+        catch(err){
+
+        }
+    },
+}
