73284feb91bb12308d9ea9f2aacf177d82beb5ac
Michael Guedko <mjgeko@gmail.com>
2023-07-21 13:30:18 -0400

    Add links and glass class

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,12 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
+import { Box, Link } from "@chakra-ui/react";
 
 export default function Root() {
     return(
-        <Box display='flex' height='100vh' width='100vw' flexDirection='column' >
-            <Box height='24' >
-
+        <Box display='flex' height='100vh' width='100vw' flexDirection='column' sx={{backgroundImage: 'radial-gradient(circle, #000000 2px, rgba(0, 0, 0, 0) 3px);', backgroundSize: '60px 60px'}} >
+            <Box height='20' background='brand.400' display='flex' alignItems='center' justifyContent='space-between'  >
+                <QuestionOutlineIcon boxSize={8} ml={8} />
+                <Box display='flex'   alignItems='center'>
+                    <Link href="#">Product</Link>
+                    <Link href="#">Features</Link>
+                    <Link href="#">FAQ</Link>
+                    <Link href="#">Company</Link>
+                </Box>
+                <Box mr={8}>
+                    <Link href="/auth/login">Log in</Link>
+                </Box>
             </Box>
-            <Box flex='1' >
+            <Box className="glass" flex='1' >
 
             </Box>
         </Box>
