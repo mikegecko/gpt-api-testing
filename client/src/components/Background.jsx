e198a944b05d667f8f1f322a7031cdb492c2814c
Michael Guedko <mjgeko@gmail.com>
2023-07-21 17:32:13 -0400

    Tried to add cool moving effect

---
+++ client/src/components/Background.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+import _ from 'lodash';
+import { Box, useColorModeValue } from "@chakra-ui/react";
+
+export default function Background() {
+
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    const [backgroundTranslate, setBackgroundTranslate] = useState({x: 0, y: 0});
+    const bg_circle = useColorModeValue('brand.800', 'brand.100');
+
+    const handleMouseMove = (e) => {
+        const {clientX, clientY} = e;
+        setMousePosition({x: clientX, y: clientY});
+    }
+
+    useEffect(() => {
+        //Updates bg position on mouse move
+        const updateBackgroundPosition = () => {
+            const targetTranslateX = -(mousePosition.x - window.innerWidth / 2) / 20;
+            const targetTranslateY = -(mousePosition.y - window.innerHeight / 2) / 20;
+            //Smooth interpolation
+            setBackgroundTranslate(prevTranslate => ({
+                x: prevTranslate.x + (targetTranslateX - prevTranslate.x) * 0.1,
+                y: prevTranslate.y + (targetTranslateY - prevTranslate.y) * 0.1,
+            }));
+            //requestAnimationFrame(updateBackgroundPosition);
+        }
+
+        //Throttle updates
+        const throttledUpdate = _.throttle(updateBackgroundPosition, 100);
+        window.addEventListener('mousemove', throttledUpdate);
+        //requestAnimationFrame(updateBackgroundPosition);
+
+        return () => {
+            window.removeEventListener('mousemove', throttledUpdate);
+            //cancelAnimationFrame(updateBackgroundPosition);
+        };
+    }, [mousePosition])
+
+    
+
+    return(
+        
+        <Box onMouseMove={handleMouseMove} sx={{ overflow: 'hidden', zIndex: 0 ,position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: `radial-gradient(circle, ${bg_circle} 2px, rgba(0, 0, 0, 0) 3px);`, backgroundSize: '60px 60px', transform: `translate(${backgroundTranslate.x}px, ${backgroundTranslate.y}px)`, transition: 'transform 0.2s ease'}}>
+        </Box>
+    )
+}
--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,17 +1,27 @@
+import { React, useState } from 'react';
 import { ArrowForwardIcon, HamburgerIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
 import { Box, Button, IconButton, Link, Text, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { useEffect } from "react";
+import Background from '../components/Background';
+
 
 export default function Root() {
 
     const {colorMode, toggleColorMode} = useColorMode();
 
+
     const bg_circle = useColorModeValue('brand.800', 'brand.100');
     const bg_header = useColorModeValue('brand.400', 'brand.600');
     const bg_text = useColorModeValue('gray.800', 'gray.200');
     const bg_button = useColorModeValue('white', ' brand.800');
+
+    
+
+
     return(
-        <Box display='flex' height='100vh' width='100vw' flexDirection='column'  sx={{backgroundImage: `radial-gradient(circle, ${bg_circle} 2px, rgba(0, 0, 0, 0) 3px);`, backgroundSize: '60px 60px'}} >
-            <Box height='20' background={bg_header} display='flex' alignItems='center' justifyContent='space-between'  >
+        <Box display='flex' height='100vh' width='100vw' flexDirection='column' overflow='hidden' >  
+            <Background />
+            <Box zIndex={10} height='20' background={bg_header} display='flex' alignItems='center' justifyContent='space-between'  >
                 <Box display={{base: 'none', lg: 'flex'}} alignItems='center' width='10%' ml={8} >
                     <IconButton onClick={toggleColorMode} background={bg_button} aria-label="color mode" icon={<QuestionOutlineIcon />} />
                 </Box>
@@ -29,11 +39,18 @@ export default function Root() {
                     <IconButton variant='unstyled' aria-label="menu" icon={<HamburgerIcon boxSize={8} />} />
                 </Box>
             </Box>
-            <Box className="" flex='1' >
-                <Text  fontSize={{base: '4xl', md: '4xl', lg: '6xl'}} fontWeight='bold' textAlign='center' mt={6}>
+            <Box zIndex={10} flex='1' display='flex' alignItems='center' justifyContent='center' flexDirection='column' >
+                <Text  fontSize={{base: '4xl', md: '4xl', lg: '6xl'}} fontWeight='bold' textAlign='center'>
                     Welcome to the future of AI
                 </Text>
+                <Text fontSize={{base: 'xl', md: 'xl', lg: '2xl'}} fontWeight='300' textAlign='center' mb={2}>
+                    AI is changing how business works, lets change it together
+                </Text>
+                <Box>
+                    <Button color={bg_text} backdropFilter='blur(4px)' variant='solid' as='a' rightIcon={<ArrowForwardIcon />} href="#">Get Started</Button>
+                </Box>
             </Box>
         </Box>
+
     )
 }
