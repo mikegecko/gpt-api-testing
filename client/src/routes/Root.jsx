c75fb03503a6b701520d9af46735c5a1f2acae13
Michael Guedko <mjgeko@gmail.com>
2023-07-21 16:07:45 -0400

    Style colormode button

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -8,11 +8,12 @@ export default function Root() {
     const bg_circle = useColorModeValue('black', 'white');
     const bg_header = useColorModeValue('brand.400', 'brand.600');
     const bg_text = useColorModeValue('gray.800', 'gray.200');
+    const bg_button = useColorModeValue('white', ' brand.800');
     return(
         <Box display='flex' height='100vh' width='100vw' flexDirection='column' sx={{backgroundImage: `radial-gradient(circle, ${bg_circle} 2px, rgba(0, 0, 0, 0) 3px);`, backgroundSize: '60px 60px'}} >
             <Box height='20' background={bg_header} display='flex' alignItems='center' justifyContent='space-between'  >
                 <Box display='flex' alignItems='center' width='10%' ml={8} >
-                    <IconButton onClick={toggleColorMode} aria-label="color mode" icon={<QuestionOutlineIcon />} />
+                    <IconButton onClick={toggleColorMode} background={bg_button} aria-label="color mode" icon={<QuestionOutlineIcon />} />
                 </Box>
                 <Box display='flex' width='30%' alignItems='center' justifyContent='space-evenly'>
                     <Link fontWeight='semibold' href="#">Product</Link>
