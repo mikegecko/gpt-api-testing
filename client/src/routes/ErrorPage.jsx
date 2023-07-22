333bc553446645e96121d8647995a996c8a23f7b
Michael Guedko <mjgeko@gmail.com>
2023-07-22 12:33:26 -0400

    Add links

--- client/src/routes/ErrorPage.jsx
+++ client/src/routes/ErrorPage.jsx
@@ -7,8 +7,8 @@ export default function ErrorPage() {
             <Heading as='h1' size='4xl'>Page not found</Heading>
             <Text mt='4' fontSize='lg'>Sorry, we couldn't find the page you're looking for</Text>
             <Box mt='4' display='flex' gap='2'>
-                <Button as='a' variant="solid">Go back home</Button>
-                <Button as='a' variant="ghost">Contact Support</Button>
+                <Button as='a' variant="solid" href="/">Go back home</Button>
+                <Button as='a' variant="ghost" href="#">Contact Support</Button>
             </Box>
         </Box>
     )
