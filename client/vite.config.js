84df68f1649460b1b6c4da4674e1c828d3cc1e3a
Michael Guedko <mjgeko@gmail.com>
2023-07-22 14:17:42 -0400

    Fix proxy for dev environment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/':{
-      target: 'http://localhost:5000', //Change to URL of API
-      changeOrigin: true,
-      secure: false,
+  server:{
+    proxy: {
+      '/api':{
+        target: 'http://localhost:8080', //Change to URL of API
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
 })
