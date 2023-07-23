6b57c95be70b6401a443adefe643f5f06be12092
Michael Guedko <mjgeko@gmail.com>
2023-07-23 19:34:20 -0400

    Add user avatar to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { AddIcon, ArrowLeftIcon, ArrowRightIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
-import { Box, Button, Heading, IconButton, useDisclosure } from "@chakra-ui/react";
+import { Avatar, Box, Button, Divider, Heading, IconButton, Text, useDisclosure } from "@chakra-ui/react";
 import { AnimatePresence, motion } from "framer-motion";
 
 
@@ -8,19 +8,28 @@ export default function Sidebar() {
 
   return (
     <Box height='100%' display='flex'>
-      <IconButton position='absolute' top={'40px'} left={0} m={4} hidden={!isOpen} onClick={onToggle} aria-label="Open sidebar" icon={<ArrowRightIcon />} />
+      <IconButton zIndex={0} position='absolute' top={'48px'} left={0} m={4} hidden={!isOpen} onClick={onToggle} aria-label="Open sidebar" icon={<ArrowRightIcon />} />
       <AnimatePresence initial={false}>
         {!isOpen && (
       <motion.aside initial={{ x: '-100%' }} animate={{ x: isOpen ? '100%' : '0%', display: isOpen ? 'none' : 'block' }} exit={{ x: '-100%', transition: { duration: 0.2 } }} transition={{ duration: 0.2 }} style={{ zIndex: 0, width: '300px', position: 'relative'}}>
-        <Box maxW='300px' minW='250px' width='100%' height='100%' display='flex' flexDir='column' background='brand.600'>
+        <Box maxW='300px' minW='250px' width='100%' height='100%' display='flex' flexDir='column' justifyContent='space-between' background='brand.600'>
+            <Box>
                 <Box m={2} display='flex' flexDir='row' alignItems='center'>
-                <IconButton as='a' href="/" icon={<QuestionOutlineIcon />} />
-                <Heading size='md' ml={2} mr={2}>Sidebar</Heading>
+                    <IconButton as='a' href="/" icon={<QuestionOutlineIcon />} />
+                    <Heading size='md' ml={2} mr={2}>Sidebar</Heading>
                 </Box>
                 <Box display='flex' gap={2} m={2}>
-                <Button display='flex' flex='1' justifyContent='flex-start' variant='outline' gap={2} alignItems='center' leftIcon={<AddIcon />}>New Game</Button>
-                <IconButton onClick={onToggle} aria-label="close sidebar" icon={<ArrowLeftIcon />} variant='outline' />
+                    <Button display='flex' flex='1' justifyContent='flex-start' variant='outline' gap={2} alignItems='center' leftIcon={<AddIcon />}>New Game</Button>
+                    <IconButton onClick={onToggle} aria-label="close sidebar" icon={<ArrowLeftIcon />} variant='outline' />
                 </Box>
+            </Box>
+            <Box display='flex' flexDir='column' gap={2} p={2}>
+                <Divider />
+                <Button display='flex' justifyContent='flex-start' gap={2} alignItems='center' variant='ghost'>
+                    <Avatar size='sm' name="temp" src='https://bit.ly/broken-link' />
+                    <Text p={2} mt={0.25}>Username</Text>
+                </Button>
+            </Box>
         </Box>
       </motion.aside>
         )}
