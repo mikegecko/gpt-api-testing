86879623676fa2a6614e4040f05caa4da168f414
Michael Guedko <mjgeko@gmail.com>
2023-07-22 21:27:45 -0400

    Add basic testing interface for gpt completions

--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -1,33 +1,73 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Input, Text } from "@chakra-ui/react";
 import { router } from "../main";
-import { decodeToken, logout, verify } from "../utils/api";
+import { decodeToken, gptChatCompletion, logout, verify } from "../utils/api";
 import { useEffect, useState } from "react";
 
 export default function Dashboard() {
 
     const [tokenInfo, setTokenInfo] = useState(null);
+    const [input, setInput] = useState("");
+    const [response, setResponse] = useState(null);
+    const [messages, setMessages] = useState([]);
+
+    const tokenInfoString = JSON.stringify(tokenInfo);
 
     const handleLogout = () => {
         logout();
     }
 
+    const addUserMessage = (message) => {
+        const formattedMessage = { "role": "user", "content": message };
+        setMessages([...messages, formattedMessage]);
+    }
+
+    const handleSend = () => { 
+        addUserMessage(input);
+        setInput("");
+        gptChatCompletion(messages, tokenInfo.apiKey).then(res => {setResponse(res); console.log(res);})
+    }
+
+
     useEffect(() => {
         const jwt = localStorage.getItem('gptapi-token');
         decodeToken(jwt).then(res => {setTokenInfo(res);})
+
+        const addSystemMessage = () => {
+            setMessages([...messages, {"role": "system", "content": "You are a evil helpful assistant who only speaks like a gangster from the hood."}]);
+        }
+        addSystemMessage();
     },[])
-
+    // useEffect(() => {
+    //     console.log(tokenInfo);
+    // }, [tokenInfo])
     useEffect(() => {
-        console.log(tokenInfo);
-    }, [tokenInfo])
-
-    const tokenInfoString = JSON.stringify(tokenInfo);
+        const addResponseMessage = () => {
+            if(response != null){
+                const message = response.choices[0].message;
+                setMessages([...messages, message]);
+            }
+        }   
+        addResponseMessage();
+    }, [response])
+    useEffect(() => {
+        console.log(messages);
+    }, [messages])
 
     return(
-        <Box display='flex' height='100vh' >
+        <Box display='flex' height='100vh' width='100vw' flexDir='column' >
             <Box flex='1'>
                 <h1>Dashboard</h1>
                 <Button onClick={handleLogout}>Logout</Button>
                 <Text>{tokenInfoString}</Text>
+            </Box>
+            <Box>
+                {messages.map((message, index) => {
+                    return(
+                        <Text key={index}>{message.role}:{message.content}</Text>
+                    )
+                })}
+                <Input onChange={(e) => setInput(e.target.value)} value={input} placeholder="Send message" />
+                <Button onClick={handleSend}>Send</Button>
             </Box>
         </Box>
     )
