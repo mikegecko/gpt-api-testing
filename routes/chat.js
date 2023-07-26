fc36df92a9f367adc5fc24a181fab7ebd6813dfb
Michael Guedko <mjgeko@gmail.com>
2023-07-26 13:45:31 -0400

    Change endpoint name

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const chatController = require('../controllers/chatController');
 
 //Proxy all requests to the OpenAi chat completions API
-router.post('/chat', chatController.chatRequest);
+router.post('/request', chatController.chatRequest);
 
 module.exports = router;
 
