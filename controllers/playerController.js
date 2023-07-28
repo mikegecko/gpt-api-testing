af5b389e3f4e26bda09217b0ae8c48c53be3c5f8
Michael Guedko <mjgeko@gmail.com>
2023-07-28 12:58:43 -0400

    Add methods for player

---
+++ controllers/playerController.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const Player = require('../models/player');
+const gpt_functions = require("../utils/GPT_functions");
+
+module.exports = {
+    createPlayer: async (req, res) => {
+
+    },
+    getPlayer: async (req, res) => {
+
+    },
+    updatePlayer: async (req, res) => {
+
+    },
+    deletePlayer: async (req, res) => {
+        
+    },
+}
+
