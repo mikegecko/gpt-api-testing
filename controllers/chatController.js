04de9024cba89228d2dc3ef643333e6de6c7782f
Michael Guedko <mjgeko@gmail.com>
2023-07-27 20:17:51 -0400

    Add gpt_functions
        - Add todos
        - Add function call into proxy
        - Add comments

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const gpt_functions = require("../utils/GPT_functions");
 module.exports = {
   chatRequest: async (req, res, next) => {
     try {
@@ -9,8 +10,8 @@ module.exports = {
         {
           model: "gpt-3.5-turbo",
           messages: req.body.messages, //Formatted messages example [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": "Hello!"}]
-          functions: [], //Functions count towards context length
-          functions_call: "auto", //Valid values are 'auto' , 'none' & specified(force call)
+          //functions: gpt_functions, //Functions count towards context length
+          //functions_call: "auto", //Valid values are 'auto' , 'none' & specified(force call)
           temperature: 0.7,
         },
         {
@@ -21,6 +22,7 @@ module.exports = {
         }
       );
       const data = response.data;
+      //Check response data to see if GPT called a function, if so execute it and 
       return res.json(data);
     } catch (error) {
       return next(error);
---
+++ utils/gpt_functions.js
@@ -0,0 +1,13 @@
+const gpt_functions = [
+    {
+        name: 'get_player_data',
+        description: "Get the player's data",
+        parameters: {
+            type: "object",
+            properties: {
+                
+            }
+        }
+    },
+];
+module.exports = gpt_functions;
