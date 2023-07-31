0544125e4f156ed3e57d077bddacead1af30e2fc
Michael Guedko <mjgeko@gmail.com>
2023-07-31 16:25:37 -0400

    Add carousel & recently played section     - Make header sticky

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,8 +1,9 @@
-import { ArrowForwardIcon, HamburgerIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
+import { ArrowForwardIcon, HamburgerIcon, QuestionOutlineIcon, TimeIcon } from "@chakra-ui/icons";
 import { Avatar, Box, Button, IconButton, Link, Text, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import Background from '../components/Background';
 import { verify } from '../utils/api';
 import { useLoaderData } from 'react-router-dom';
+import ImageCarousel from "../components/ImageCarousel";
 
 
 export default function Root() {
@@ -13,11 +14,17 @@ export default function Root() {
     const bg_header = useColorModeValue('brand.400', 'brand.600');
     const bg_text = useColorModeValue('gray.800', 'gray.200');
     const bg_button = useColorModeValue('white', ' brand.800');
-
+    // Replace with actual images and add in headers + text
+    const images = ["https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",]
 
     return(
-        <Box display='flex' height='100vh' width='100vw' flexDirection='column' overflow='hidden' >  
-            <Box zIndex={10} height='60px' background={bg_header} display='flex' alignItems='center' justifyContent='space-between' >
+        <Box display='flex' height='100%' width='100%' flexDirection='column' >  
+            <Box zIndex={10} height='60px' width='100%' background={bg_header} position='sticky' sx={{top: 0, left: 0, zIndex: 15}} display='flex' alignItems='center' justifyContent='space-between' >
                 <Box display={{base: 'none', lg: 'flex'}} alignItems='center' width='10%' ml={8} >
                     <IconButton onClick={toggleColorMode} background={bg_button} aria-label="color mode" icon={<QuestionOutlineIcon />} />
                 </Box>
@@ -35,15 +42,23 @@ export default function Root() {
                     <IconButton variant='unstyled' aria-label="menu" icon={<HamburgerIcon boxSize={8} />} />
                 </Box>
             </Box>
-            <Box zIndex={10} flex='1' display='flex' alignItems='center' justifyContent='center' flexDirection='column' >
+            <Box zIndex={10} flex='1' display='flex' alignItems='center' justifyContent='flex-start' flexDirection='column' mt={4} >
+                <ImageCarousel images={images} />
                 <Text  fontSize={{base: '4xl', md: '4xl', lg: '6xl'}} fontWeight='bold' textAlign='center'>
                     Welcome to the GPT Dungeon
                 </Text>
-                <Text fontSize={{base: 'md', md: 'md', lg: 'xl'}} mr={24} ml={24} fontWeight='300' textAlign='center' mb={8}>
+                <Text fontSize={{base: 'md', md: 'md', lg: 'xl'}} mr={24} ml={24} fontWeight='300' textAlign='center' mb={8} maxW="1037px">
                 Embark on an unforgettable adventure like never before with GPT Dungeon - the ultimate AI-powered text-based game. Immerse yourself in a world of limitless possibilities, where your choices shape the story. Are you ready to test your wits and unravel the mysteries of the GPT Dungeon? Prepare for a thrilling journey into the unknown. Begin your adventure now!
                 </Text>
                 <Box>
                     <Button color={bg_text} backdropFilter='blur(4px)' variant='solid' as='a' rightIcon={<ArrowForwardIcon />} href="#">Get Started</Button>
+                </Box>
+                <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" mt={8}>
+                    <Box display='flex' flexDir='row' alignItems='center' justifyContent='center' width='100%' mb={8} gap={4}>
+                    <TimeIcon boxSize={6}  />
+                    <Text fontSize={{base: 'md', md: 'md', lg: 'xl'}} mt={8} fontWeight='300' textAlign='center' mb={8} >Recently Played</Text>
+                    </Box>
+                    
                 </Box>
             </Box>
         </Box>
