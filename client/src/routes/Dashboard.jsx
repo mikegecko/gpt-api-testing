e90ce5bc891a36645bd70402b6336ee2de96b896
Michael Guedko <mjgeko@gmail.com>
2023-08-01 20:20:22 -0400

    Fix title, add dashboard loader logic

--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -3,9 +3,10 @@ import { router } from "../main";
 import { decodeToken, verify } from "../utils/api";
 import { useLoaderData } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
+import Header from "../components/Header";
 
 export default function Dashboard() {
-    const encodedToken = useLoaderData();
+    const loaderData = useLoaderData();
     const [tokenInfo, setTokenInfo] = useState(null);
 
 
@@ -16,8 +17,8 @@ export default function Dashboard() {
     },[])
 
     return(
-        <Box>
-            
+        <Box display='flex' minH='100vh' minW='100vw' flexDir='row' backdropFilter='blur(100px)'>
+            <Header jwtValid={loaderData.jwtValid} />
         </Box>
     )
 
@@ -26,12 +27,51 @@ export default function Dashboard() {
 export async function dashboardLoader () {
     //Redirects to login if no token is found
     const jwt = localStorage.getItem('gptapi-token');
+    let jwtValid = false;
     if(!jwt){
     router.navigate('/auth/login');
     }
+    // if(jwt){
+    //     //FIX: res.success is undefined when token is expired
+    //     verify(localStorage.getItem('gptapi-token')).then((res) => {res.success ? console.log("Token is valid") : router.navigate('/auth/login')}).catch(err => { router.navigate('/auth/login'); console.log(err)})
+    // }
     if(jwt){
-        //FIX: res.success is undefined when token is expired
-        verify(localStorage.getItem('gptapi-token')).then((res) => {res.success ? console.log("Token is valid") : router.navigate('/auth/login')}).catch(err => { router.navigate('/auth/login'); console.log(err)})
+        try {
+            const res = await verify(localStorage.getItem('gptapi-token'));
+            //console.log(res);
+            if(res === undefined){
+                //Token is invalid
+                router.navigate('/auth/login')
+                return({
+                    jwtValid: jwtValid,
+                    jwt: jwt,
+                })
+            }
+            if(!res.success){
+                //Token is invalid
+                router.navigate('/auth/login')
+                return({
+                    jwtValid: jwtValid,
+                    jwt: jwt,
+                })
+            }
+            if(res.success){
+                //Token is valid, show avatar and hide login button
+                jwtValid = true;
+                return({
+                    jwtValid: jwtValid,
+                    jwt: jwt,
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            //Token is invalid
+            router.navigate('/auth/login')
+            return({
+                jwtValid: jwtValid,
+                jwt: jwt,
+            })
+        }
     }
-    return jwt;
+    return ({jwt: jwt, jwtValid: jwtValid});
 }
