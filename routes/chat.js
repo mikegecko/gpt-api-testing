a8051409d120e9faa45141fb0c3e2c3cf475b731
Michael Guedko <mjgeko@gmail.com>
2023-08-03 13:26:26 -0400

    Add new api route for convos

--- routes/chat.js
+++ routes/chat.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
 const authController = require('../controllers/authController');
+const convoController = require('../controllers/convoController');
 
 //Proxy all requests to the OpenAi chat completions API
 router.post('/request', authController.verifyToken, chatController.chatRequest);
+
+router.post('/conversation', authController.verifyToken, convoController.createConvo);
 
 module.exports = router;
 
