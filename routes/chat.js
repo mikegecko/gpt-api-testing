71a598dd4f0d19570130069514272eea23d41158
Michael Guedko <mjgeko@gmail.com>
2023-08-05 13:11:30 -0400

    Add routes for delete and get by user

--- routes/chat.js
+++ routes/chat.js
@@ -6,10 +6,14 @@ const convoController = require('../controllers/convoController');
 
 //Proxy all requests to the OpenAi chat completions API
 router.post('/request', authController.verifyToken, chatController.chatRequest);
-
+//Create a conversation
 router.post('/conversation', authController.verifyToken, convoController.createConvo);
-
+//Get a conversation by id
 router.get('/conversation/:id', authController.verifyToken, convoController.getConvo);
+//Get all user conversations
+router.get('/conversation', authController.verifyToken, convoController.getCovosByUser);
+//Delete a conversation by id
+router.delete('/conversation/:id', authController.verifyToken, convoController.deleteConvo);
 
 module.exports = router;
 
