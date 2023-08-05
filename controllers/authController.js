93af8085dad2f0ee23cc538ba0feb5ad7f33553d
Michael Guedko <mjgeko@gmail.com>
2023-08-05 15:32:51 -0400

    Add todos

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,6 +28,7 @@ module.exports = {
         }
     },
     deleteUser: async (req, res, next) => {
+        //Make this more secure
         try{
             const token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
