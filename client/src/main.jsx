9d4a3345310af48f332d853eb72cca414e6eaee0
Michael Guedko <mjgeko@gmail.com>
2023-08-05 14:59:57 -0400

    Attempt to change bg using colormode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Root, { rootLoader } from './routes/Root'
-import Login from './routes/Login'
-import Dashboard, { dashboardLoader } from './routes/Dashboard'
-import ErrorPage from './routes/ErrorPage'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from './utils/theme'
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root, { rootLoader } from "./routes/Root";
+import Login from "./routes/Login";
+import Dashboard, { dashboardLoader } from "./routes/Dashboard";
+import ErrorPage from "./routes/ErrorPage";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./utils/theme";
 // Supports weights 100-900
-import '@fontsource-variable/inter';
-import Signup from './routes/Signup'
+import "@fontsource-variable/inter";
+import Signup from "./routes/Signup";
+import NewAdventure, { newAdventureLoader } from "./routes/NewAdventure";
+
+//Background image this is error 404 cant find the resource
+const darkbg = "url(assets/layered-steps.svg)";
+const lightbg = "url(assets/layered-steps-light.svg)";
 
 //Add in error elements & loaders
 export const router = createBrowserRouter([
@@ -35,14 +40,33 @@ export const router = createBrowserRouter([
     element: <Dashboard />,
     errorElement: <ErrorPage />,
     loader: dashboardLoader,
-  }
+  },
+  {
+    path: "/new",
+    element: <NewAdventure />,
+    errorElement: <ErrorPage />,
+    loader: newAdventureLoader,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <RouterProvider className="App" router={router} />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+function App() {
+  const [bgImage, setBgImage] = useState(darkbg);
+
+  useEffect(() => {
+    const colorMode = localStorage.getItem("chakra-ui-color-mode");
+    setBgImage(colorMode === "light" ? lightbg : darkbg);
+  }, []);
+
+  return (
+    <div id="root">
+      <React.StrictMode>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <RouterProvider className="App" router={router} />
+        </ChakraProvider>
+      </React.StrictMode>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
