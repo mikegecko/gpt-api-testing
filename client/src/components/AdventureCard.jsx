7b67b376110a6be99474b1d93fac8f53ce08d8e4
Michael Guedko <mjgeko@gmail.com>
2023-08-05 11:54:30 -0400

    Add adventure card styling and anims

--- client/src/components/AdventureCard.jsx
+++ client/src/components/AdventureCard.jsx
@@ -1,36 +1,54 @@
-import { PropTypes } from 'prop-types';
-import { Box, Text } from "@chakra-ui/react";
-import { useState } from 'react';
+import { PropTypes } from "prop-types";
+import { Box, Divider, Text, useTheme } from "@chakra-ui/react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
-export default function AdventureCard({onClick, cardHeader, }){
-    const [isHovered, setIsHovered] = useState(false);
+export default function AdventureCard({ onClick, title, description }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const theme = useTheme();
 
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    }
-    const handleMouseLeave = () => {
-        setIsHovered(false);
-    }
-    const boxtStyle = {
-        height: '300px',
-        width: '300px',
-        backgroundColor: isHovered ? 'lightblue' : 'brand.500',
-        borderRadius: '10px',
-        padding: '1rem',
-        cursor: 'pointer',
-      }
-    return(
-        <Box style={boxtStyle} 
-            onMouseEnter={handleMouseEnter} 
-            onMouseLeave={handleMouseLeave}
-            onClick={onClick}
-         >
-            <Text>{cardHeader}</Text>
-        </Box>
-    )
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const boxVariants = {
+    initial: { backgroundColor: theme.colors.brand[500] },
+    hover: { backgroundColor: theme.colors.brand[400] },
+  };
+
+  const boxStyle = {
+    borderRadius: "10px",
+    padding: "0rem",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    flexDirection: "column",
+  };
+  return (
+    <motion.div initial='initial' whileHover='hover' onClick={onClick} style={{cursor: 'pointer'}}>
+      <Box
+        display={{base: 'flex', lg: 'flex'}}
+        height={{base: '100%', lg: '300px'}}
+        width={{base: '100%', lg: '300px'}}
+        as={motion.div}
+        variants={boxVariants}
+        style={boxStyle}
+      >
+        <Divider w='100%' />
+        <Text fontSize='lg' fontWeight='bold' color='white'>
+          {description}
+        </Text>
+        <Text fontSize='lg' fontWeight='bold' padding='.5rem 1rem .5rem 1rem'>{title}</Text>
+      </Box>
+    </motion.div>
+  );
 }
 
 AdventureCard.propTypes = {
-    cardHeader: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
-}
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  description: PropTypes.string,
+};
